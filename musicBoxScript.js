d4b39195011af540cfc20d002cc95ec151bef93d
Cal <calcorbin@gmail.com>
2016-03-06 18:27:47 -0600

    Musical Boxes
    The musical box coder project from Google I am working on.

---
+++ musicBoxScript.js
@@ -0,0 +1,30 @@
+$(document).ready( function() {
+	$('#c').mousedown(function(){
+	    cNote.currentTime = 0;
+	    cNote.play();
+	});
+	$('#d').mousedown(function(){
+	    dNote.currentTime = 0;
+	    dNote.play();
+	});
+	$('#e').mousedown(function(){
+	    eNote.currentTime = 0;
+	    eNote.play();
+	});
+	$('#f').mousedown(function(){
+	    fNote.currentTime = 0;
+	    fNote.play();
+	});
+	$('#g').mousedown(function(){
+	    gNote.currentTime = 0;
+	    gNote.play();
+	});
+	$('#a').mousedown(function(){
+	    aNote.currentTime = 0;
+	    aNote.play();
+	});
+	$('#b').mousedown(function(){
+	    bNote.currentTime = 0;
+	    bNote.play();
+	});
+});
